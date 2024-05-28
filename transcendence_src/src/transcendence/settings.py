0667a95e87a8c484d4bf4ea1523437525c34a731
CrispLake <eemilmajuri@gmail.com>
2024-05-28 10:51:06 +0300

    Moved secret key from settings to env file

--- transcendence_src/src/transcendence/settings.py
+++ transcendence_src/src/transcendence/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wd2zi*)@_t803&!glvqtqo7sp=vi%w(vncls@_+@3t9w*xw)c&'
+SECRET_KEY = os.environ['SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

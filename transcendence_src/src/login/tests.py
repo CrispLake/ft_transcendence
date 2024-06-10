3d5c3600517ef3e9f3c5f629a9fcc02c277699fe
Eemil Majuri <emajuri@c1r1p11.hive.fi>
2024-06-10 12:03:54 +0300

    Added login to installed apps, created migrations and created a test

--- transcendence_src/src/login/tests.py
+++ transcendence_src/src/login/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import User
 
 # Create your tests here.
+
+class UsernameTests(TestCase):
+    def test_username_exists(self):
+        User.objects.create(username='TestUser', password='abc')
+        self.assertIs(User.objects.filter(username='TestUser').exists(), True)
--- transcendence_src/src/login/views.py
+++ transcendence_src/src/login/views.py
@@ -1,34 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from login.models import User
-
-# Create your views here.
-# def index(request):
-#     return render(request, "singlepage/index.html")
-
-# def section(request, num):
-#     if 1 <= num <= 3:
-#         return HttpResponse(texts[num-1])
-#     else:
-#         raise Http404("No such section")
-
-# def test_add(request):
-#     text = request.GET.get('text', 'hello')  # Default to 'hello' if no text provided
-#     Test.objects.create(text=text)
-#     return HttpResponse(status=201)
-
-
-# def test_display(request):
-#     tests = Test.objects.all().values('text')
-#     tests_list = list(tests)
-
-#     html_content = "<ul>"
-#     for test in tests_list:
-#         html_content += f"<li>{test['text']}</li>"
-#     html_content += "</ul>"
-
-#     return HttpResponse(html_content)
-
+from .models import User
 
 # TODO create validates
 def validate_username(username):

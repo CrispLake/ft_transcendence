c0f5ea4e2ea4e1f41d9ac6b8d2a33a0732b42383
Eemil Majuri <emajuri@c1r1p11.hive.fi>
2024-06-10 12:03:18 +0300

    Fixed model

--- transcendence_src/src/login/models.py
+++ transcendence_src/src/login/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    username = models.charField(max_length=100)
-    password = models.charField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)

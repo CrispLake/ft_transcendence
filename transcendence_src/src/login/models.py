6eb49e5aa65d7d82b4b89565af40bd0183662f61
Eemil Majuri <emajuri@c1r1p11.hive.fi>
2024-06-10 18:12:33 +0300

    added comments

--- transcendence_src/src/login/models.py
+++ transcendence_src/src/login/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+# stats
+# pfp
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)

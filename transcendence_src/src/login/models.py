74406179355c56e3742cd9e8901acb063533ed69
CrispLake <eemilmajuri@gmail.com>
2024-06-11 14:05:00 +0300

    Added more stuff to User model. Modified django container to be open in compose. Added Pillow on requirements for imagefield in model

--- transcendence_src/src/login/models.py
+++ transcendence_src/src/login/models.py
@@ -2,20 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-# class password(models.Model):
-#     password = models.CharField(max_length=50)
-#     user = models.ForeignKey("login.User", related_name="passwd")
+class Match(models.Model):
+    winners = models.CharField(max_length=110)
+    losers = models.CharField(max_length=110)
+    date = models.DateTimeField()
 
-# class User(models.Model):
-#     username = models.CharField(max_length=50)
-#     pfp = models.ImageField()
-#     wins = models.IntegerField()
-#     losses = models.IntegerField()
-#     sessionid?
-#     gamehistory?
-
-# stats
-# pfp
 class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+    pfp = models.ImageField()
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+    # sessionid = models.CharField
+    # gamehistory?
+    # friends

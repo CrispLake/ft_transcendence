a549e1fdc66fe1d24a326cdf2a9344c4e7ff41a5
Eemil Majuri <emajuri@c1r1p11.hive.fi>
2024-06-11 13:30:39 +0300

    added ideas for the user table

--- transcendence_src/src/login/models.py
+++ transcendence_src/src/login/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+# class password(models.Model):
+#     password = models.CharField(max_length=50)
+#     user = models.ForeignKey("login.User", related_name="passwd")
+
+# class User(models.Model):
+#     username = models.CharField(max_length=50)
+#     pfp = models.ImageField()
+#     wins = models.IntegerField()
+#     losses = models.IntegerField()
+#     sessionid?
+#     gamehistory?
+
 # stats
 # pfp
 class User(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)

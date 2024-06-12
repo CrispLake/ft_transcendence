254d39067dbb35a106316e119502fed2b9024f55
jbagger <jonathanbagger@hotmail.com>
2024-06-12 15:38:12 +0300

    Moving consts to separate file

---
+++ transcendence_src/src/pong/globals.js
@@ -0,0 +1,18 @@
+// Arena
+export const arenaLength = 25;
+export const arenaWidth = 15;
+export const wallHeight = 0.5;
+export const wallThickness = 0.2;
+export const floorThickness = 0.2;
+export const floorWidth = arenaWidth + floorThickness * 2;
+export const wallLightIntensity = 1;
+
+export const maxAngleDegrees = 20;
+
+// Paddle
+export const paddleLightIntensity = 1;
+export const paddleLength = 4;
+export const paddleHeight = wallHeight;
+export const paddleThickness = 0.2;
+
+

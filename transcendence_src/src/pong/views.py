a7bbc0e01dacd0a354ca320b5ab1bbc24323647b
CrispLake <eemilmajuri@gmail.com>
2024-06-12 15:07:51 +0300

    Added view to list all matches or add new one. Added urls for the view

--- transcendence_src/src/pong/views.py
+++ transcendence_src/src/pong/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from pong.models import Match
+from pong.serializers import MatchSerializer
 
 # Create your views here.
+
+@csrf_exempt
+def match_list(request):
+    """
+    List all matches or add new
+    """
+    if request.method == 'GET':
+        matches = Match.objects.all()
+        serializer = MatchSerializer(matches, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = MatchSerializer(data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)

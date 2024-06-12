b7b0697f49e71154948ef455567b55ea7b2cf5ad
CrispLake <eemilmajuri@gmail.com>
2024-06-12 12:19:04 +0300

    Created a model using the base user model

--- transcendence_src/src/login/models.py
+++ transcendence_src/src/login/models.py
@@ -1,18 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Match(models.Model):
-    winners = models.CharField(max_length=110)
-    losers = models.CharField(max_length=110)
-    date = models.DateTimeField()
+# class Match(models.Model):
+#     winners = models.CharField(max_length=110)
+#     losers = models.CharField(max_length=110)
+#     date = models.DateTimeField()
 
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     pfp = models.ImageField()
     wins = models.IntegerField()
     losses = models.IntegerField()
-    # sessionid = models.CharField
     # gamehistory?
     # friends
--- transcendence_src/src/login/views.py
+++ transcendence_src/src/login/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from .models import User
+from django.contrib.auth.models import User
 
 # TODO create validates
 def validate_username(username):
@@ -10,24 +10,36 @@ def validate_password(password):
     return True
 
 def register(request):
-    username = request.POST.get('username', '')
-    if validate_username(username) == False:
-        return HttpResponse(status=501) # TODO return correct error
+    if request.method == "GET":
+        return render(request, "user/register.html") # TODO remove or make it
+    elif request.method == "POST":
+        username = request.POST.get('username', '')
+        if validate_username(username) == False:
+            return HttpResponse(status=501) # TODO return correct error
 
-    password = request.POST.get('password', '')
-    if validate_password(password) == False:
-        return HttpResponse(status=501) # TODO return correct error
+        password = request.POST.get('password', '')
+        if validate_password(password) == False:
+            return HttpResponse(status=501) # TODO return correct error
+        
+        user = User.objects.create(username, "", password) # empty string to put empty email for user
+        user.account.wins = 0
+        user.account.losses = 0
+        # user.account.pfp = defaultpfp #TODO Add default pfp to the user on creation
+        user.save()
+        return HttpResponse(status=201)
 
-    User.objects.create(username=username, password=password)
-    return HttpResponse(status=201)
+# def login(request):
+#     username = request.POST.get('username', '')
+#     if User.objects.filter(username=username).exists() == False:
+#         return HttpResponse(status=501) # TODO incorrect username/password
 
-def login(request):
-    username = request.POST.get('username', '')
-    if User.objects.filter(username=username).exists() == False:
-        return HttpResponse(status=501) # TODO incorrect username/password
+#     password = request.POST.get('password', '')
+#     if User.objects.get(username=username).password != password:
+#         return HttpResponse(status=501) # TODO incorrect username/password
 
-    password = request.POST.get('password', '')
-    if User.objects.get(username=username).password != password:
-        return HttpResponse(status=501) # TODO incorrect username/password
+#     return HttpResponse(status=201)
 
-    return HttpResponse(status=201)
+def password_change(request):
+    user = User.objects.get(username=request.POST.get('username'))
+    user.set_password(request.POST.get('password'))
+    user.save()

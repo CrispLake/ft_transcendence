c241241348ba2c90b9f446bc6fa93f581e4769d5
CrispLake <eemilmajuri@gmail.com>
2024-06-13 18:51:58 +0300

    Removed email from requests

--- transcendence_src/src/login/serializers.py
+++ transcendence_src/src/login/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password', 'email']
+        fields = ['username', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }

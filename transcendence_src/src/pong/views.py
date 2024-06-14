ccd6f1034c7d8a32f7c48d97e76a888f3153ba50
CrispLake <eemilmajuri@gmail.com>
2024-06-14 16:57:56 +0300

    Protected the match view to only authorized users. Fixed settings not having token auth as default

--- transcendence_src/src/pong/views.py
+++ transcendence_src/src/pong/views.py
@@ -1,10 +1,12 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from pong.models import Match
 from pong.serializers import MatchSerializer
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def match(request):
     if request.method == 'GET':
         matches = Match.objects.all()

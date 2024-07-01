8d46557522726ccc94ec3b473f9b6117e3d11efc
CrispLake <eemilmajuri@gmail.com>
2024-07-01 12:14:48 +0300

    Added requirement to be authenticated when viewing all the players

--- transcendence_src/src/login/views.py
+++ transcendence_src/src/login/views.py
@@ -1,8 +1,8 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from login.models import Account
-from login.serializers import AccountSerializer
+from login.serializers import AccountSerializer, UserSerializer, PasswordSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -24,6 +24,9 @@ class CustomAuthToken(ObtainAuthToken):
 @api_view(['GET', 'POST'])
 def register(request):
     if request.method == "GET":
+        if not request.user.is_authenticated:
+            return Response({'detail': 'Authentication required for GET requests.'}, status=status.HTTP_403_FORBIDDEN)
+        
         accounts = Account.objects.all()
         serializer = AccountSerializer(accounts, many=True)
         return Response(serializer.data)
@@ -42,3 +45,36 @@ def register(request):
 def get_csrf_token(request):
     token = get_token(request)
     return Response({'csrfToken': token})
+
+@api_view(['PUT'])
+@permission_classes(['IsAuthenticated'])
+def update_account(request):
+    account = request.user.account
+    serializer = AccountSerializer(account, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes(['IsAuthenticated'])
+def update_user(request):
+    user = request.user
+    serializer = UserSerializer(user, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes(['IsAuthenticated'])
+def change_password(request):
+    user = request.user
+    serializer = PasswordSerializer(data=request.data)
+    if serializer.is_valid():
+        if not user.check_password(serializer.data.get("old_password")):
+            return Response({"old_password": "Wrong password."}, status=status.HTTP_400_BAD_REQUEST)
+        user.set_password(serializer.data.get("new_password"))
+        user.save()
+        return Response({"status": "password set"}, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

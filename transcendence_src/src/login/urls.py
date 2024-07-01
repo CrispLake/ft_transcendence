e39fbd46ebd838ddc65e625200ccb9038866e0a1
CrispLake <eemilmajuri@gmail.com>
2024-07-01 14:06:31 +0300

    Changed url for username change to be more obvious

--- transcendence_src/src/login/urls.py
+++ transcendence_src/src/login/urls.py
@@ -6,6 +6,6 @@ urlpatterns  = [
     path('login', views.CustomAuthToken.as_view()),
     path('csrf-token', views.get_csrf_token),
     path('account/update', views.update_account, name='update-account'),
-    path('user/update', views.update_user, name='update-user'),
+    path('user/change-username', views.update_user, name='update-user'),
     path('user/change-password', views.change_password, name='change-password'),
 ]

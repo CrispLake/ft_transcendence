7eab1988beb288d419e5a53c909905ca4386adf8
CrispLake <eemilmajuri@gmail.com>
2024-07-02 14:06:18 +0300

    Added api for adding and fetching pong-4p matches. Modified the endpoint urls for 2p

--- transcendence_src/src/pong/models.py
+++ transcendence_src/src/pong/models.py
@@ -12,3 +12,21 @@ class Match(models.Model):
 
     class Meta:
         ordering = ['date']
+
+class Pong_4p(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+
+    player1 = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="pong_4p_p1", null=True, blank=True)
+    player1Score = models.IntegerField(default=0)
+
+    player2 = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="pong_4p_p2", null=True, blank=True)
+    player2Score = models.IntegerField(default=0)
+
+    player3 = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="pong_4p_p3", null=True, blank=True)
+    player3Score = models.IntegerField(default=0)
+
+    player4 = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="pong_4p_p4", null=True, blank=True)
+    player4Score = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['date']
--- transcendence_src/src/pong/views.py
+++ transcendence_src/src/pong/views.py
@@ -4,18 +4,15 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from .authentication import MultiTokenAuthentication
-from pong.models import Match
-from pong.serializers import MatchSerializer
+from pong.models import Match, Pong_4p
+from pong.serializers import MatchSerializer, Pong4PSerializer
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 @authentication_classes([MultiTokenAuthentication])
-def match(request, player_id=None):
+def pong_2p(request, player_id=None):
     if request.method == 'GET':
         if player_id is not None:
-            if request.user.id != player_id:
-                return Response({'detail': 'You do not have permission to view these matches.'}, status=status.HTTP_403_FORBIDDEN)
-
             matches = Match.objects.filter(player1_id=player_id) | Match.objects.filter(player2_id=player_id)
         else:
             matches = Match.objects.all()
@@ -43,6 +40,52 @@ def match(request, player_id=None):
                 return Response({'detail': 'Invalid player ID or unauthorized.'}, status=status.HTTP_403_FORBIDDEN)
 
         serializer = MatchSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def check_unique_ids(a, b, c, d):
+    ids = [a, b, c, d]
+    non_none_ids = [id for id in ids if id is not None]
+    return len(non_none_ids) == len(set(non_none_ids))
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([MultiTokenAuthentication])
+def pong_4p(request, player_id=None):
+    if request.method == 'GET':
+        if player_id is not None:
+            matches = Pong_4p.objects.filter(player1_id=player_id) | Pong_4p.objects.filter(player2_id=player_id) | Pong_4p.objects.filter(player3_id=player_id) | Pong_4p.objects.filter(player4_id=player_id)
+        else:
+            matches = Pong_4p.objects.all()
+
+        serializer = Pong4PSerializer(matches, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        player1_id = request.data.get('player1')
+        player2_id = request.data.get('player2')
+        player3_id = request.data.get('player3')
+        player4_id = request.data.get('player4')
+
+        if player1_id is None:
+            return Response({'detail': 'Player ID is required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if check_unique_ids(player1_id, player2_id, player3_id, player4_id) is False:
+            return Response({'detail': 'Player IDs can not be same'}, status=status.HTTP_400_BAD_REQUEST)
+
+        tokens = [token.strip() for token in request.headers.get('Authorization', '').replace('Token ', '').split(',')]
+        valid_user_ids = [user.id for user in User.objects.filter(auth_token__key__in=tokens)]
+
+        if player1_id not in valid_user_ids:
+            return Response({'detail': 'Invalid player ID or unauthorized.'}, status=status.HTTP_403_FORBIDDEN)
+        if player2_id is not None:
+            if player2_id not in valid_user_ids:
+                return Response({'detail': 'Invalid player ID or unauthorized.'}, status=status.HTTP_403_FORBIDDEN)
+
+        serializer = Pong4PSerializer(data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)

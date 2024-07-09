2689618b079789d59fd9a7a28d22d8ebb929a640
CrispLake <eemilmajuri@gmail.com>
2024-07-09 14:06:43 +0300

    Added profile GET by id

--- transcendence_src/src/login/urls.py
+++ transcendence_src/src/login/urls.py
@@ -5,11 +5,15 @@ urlpatterns  = [
     path('register', views.register),
     path('login', views.CustomAuthToken.as_view()),
     path('csrf-token', views.get_csrf_token),
+
     path('account/update', views.update_account, name='update-account'),
-    path('user/change-username', views.update_user, name='update-user'),
-    path('user/change-password', views.change_password, name='change-password'),
+    path('account/change-username', views.update_user, name='update-user'),
+    path('account/change-password', views.change_password, name='change-password'),
+    path('account/<int:id>', views.profile),
+    path('account', views.profile),
+
     path('friend-request/send', views.send_friend_request, name='send-friend-request'),
     path('friend-request/respond/<int:request_id>', views.respond_to_friend_request, name='respond-to-friend-request'),
     path('friend-request/list', views.request_list),
-    path('friend-remove/<int:remove_id>', views.remove_friend)
+    path('friend-remove/<int:remove_id>', views.remove_friend),
 ]

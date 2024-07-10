8bb6dd50ee04ac9299b6733a63fb68d25a92a04f
CrispLake <eemilmajuri@gmail.com>
2024-07-10 17:24:40 +0300

    Added 2 forms

--- frontend/app/src/static/js/views/Settings.js
+++ frontend/app/src/static/js/views/Settings.js
@@ -21,12 +21,50 @@ export default class extends AbstractView {
         this.profileURL = 'http://localhost:8000/account';
     }
 
+    async UsernameHandler(event) {
+        event.preventDefault();
+    }
+
     async getHtml() {
         return `
             <div class="settings-div">
                 <div class="settings-card">
-                    <h1 class="font-heading">Settings</h1>
 
+                        <div class="change-form">
+                            <h2>Change Username</h2>
+                            <form id="change-username-form" action="" method="">
+
+                            <label class="font-text" for="username">username:</label>
+                            <input class="font-text login-input" type="text" id="username" name="username" required><br><br>
+
+                            <button class="font-sub change-username-submit-button" id="changeUsernameSubmitButton" type="submit">
+                                <div class="text-holder">
+                                <span id="change-username-button-text">Change</span>
+                                </div>
+                            </button>
+
+                            <div id="notification-div"></div>
+
+                            </form>
+                        </div>
+
+                        <div class="change-form">
+                            <h2>Change Password</h2>
+                            <form id="change-password-form" action="" method="">
+
+                            <label class="font-text" for="password">password:</label>
+                            <input class="font-text login-input" type="text" id="password" name="password" required><br><br>
+
+                            <button class="font-sub change-password-submit-button" id="changepasswordSubmitButton" type="submit">
+                                <div class="text-holder">
+                                <span id="change-password-button-text">Change</span>
+                                </div>
+                            </button>
+
+                            <div id="notification-div"></div>
+
+                            </form>
+                        </div>
                 </div>
             </div>
         `;

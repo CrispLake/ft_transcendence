97f2972b672ff0887a677148680a5326ae175433
Eemil Majuri <eemilmajuri@gmail.com>
2024-07-11 16:00:25 +0300

    Backend modify (#17)
    * Added missing auth token check for 3rd and 4th player
    
    * Added wins and losses changing after adding a match

--- transcendence_src/src/gonp/views.py
+++ transcendence_src/src/gonp/views.py
@@ -3,9 +3,20 @@ from rest_framework.decorators import api_view, permission_classes, authenticati
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
+from django.db.models import F
 from pong.authentication import MultiTokenAuthentication
 from .models import Gonp_4p, Gonp_2p
+from login.models import Account
 from .serializers import Gonp2PSerializer, Gonp4PSerializer
+
+def update_score(player_id, my_score):
+    if player_id is not None:
+        player = Account.objects.get(id=player_id)
+        if my_score is not 0:
+            player.wins = F('wins') + 1
+        else:
+            player.losses = F('losses') + 1
+        player.save()
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
@@ -42,6 +53,13 @@ def gonp_2p(request, player_id=None):
         serializer = Gonp2PSerializer(data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
+
+            player1_score = serializer.data['player1Score']
+            player2_score = serializer.data['player2Score']
+
+            update_score(player1_id, player1_score)
+            update_score(player2_id, player2_score)
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -81,13 +99,33 @@ def gonp_4p(request, player_id=None):
 
         if player1_id not in valid_user_ids:
             return Response({'detail': 'Invalid player ID or unauthorized.'}, status=status.HTTP_403_FORBIDDEN)
+
         if player2_id is not None:
             if player2_id not in valid_user_ids:
+                return Response({'detail': 'Invalid player ID or unauthorized.'}, status=status.HTTP_403_FORBIDDEN)
+
+        if player3_id is not None:
+            if player3_id not in valid_user_ids:
+                return Response({'detail': 'Invalid player ID or unauthorized.'}, status=status.HTTP_403_FORBIDDEN)
+
+        if player4_id is not None:
+            if player4_id not in valid_user_ids:
                 return Response({'detail': 'Invalid player ID or unauthorized.'}, status=status.HTTP_403_FORBIDDEN)
 
         serializer = Gonp4PSerializer(data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
+
+            player1_score = serializer.data['player1Score']
+            player2_score = serializer.data['player2Score']
+            player3_score = serializer.data['player3Score']
+            player4_score = serializer.data['player4Score']
+
+            update_score(player1_id, player1_score)
+            update_score(player2_id, player2_score)
+            update_score(player3_id, player3_score)
+            update_score(player4_id, player4_score)
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

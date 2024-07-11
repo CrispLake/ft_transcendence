0a0e5be439594426094e15a16387476c2b40ff9a
CrispLake <eemilmajuri@gmail.com>
2024-07-11 16:24:51 +0300

    Changed title

--- frontend/app/src/static/js/views/Settings.js
+++ frontend/app/src/static/js/views/Settings.js
@@ -1,21 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Profile.js                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/20 09:22:19 by jmykkane          #+#    #+#             */
-/*   Updated: 2024/07/09 16:09:06 by emajuri          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 import AbstractView from './AbstractView.js'; // Adjust the import as necessary
 
 export default class extends AbstractView {
     constructor(params) {
         super(params);
-        this.setTitle('Profile');
+        this.setTitle('Settings');
         this.auth = true;
         this.params = params;
         this.listeners = true;

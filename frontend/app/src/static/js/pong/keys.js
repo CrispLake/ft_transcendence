c6d308bb30de557260423306f296d38f28806828
Kinnune <eljas.kinnunen98@gmail.com>
2024-07-23 15:29:39 +0300

    Keys event fix

--- frontend/app/src/static/js/pong/keys.js
+++ frontend/app/src/static/js/pong/keys.js
@@ -1,15 +1,12 @@
-const P1_LEFT = 'a';
-const P1_RIGHT = 'd';
-const P1_BOOST = 'w';
-
-const P2_LEFT = 'ArrowLeft';
-const P2_RIGHT = 'ArrowRight';
-const P2_BOOST = 'ArrowUp';
-
-const P3_LEFT = 'j';
-const P3_RIGHT = 'l';
-const P3_BOOST = 'i';
-
-const P4_LEFT = '4';
-const P4_RIGHT = '6';
-const P4_BOOST = '8';
+export const P1_LEFT = 'a';
+export const P1_RIGHT = 'd';
+export const P1_BOOST = 'w';
+export const P2_LEFT = 'ArrowLeft';
+export const P2_RIGHT = 'ArrowRight';
+export const P2_BOOST = 'ArrowUp';
+export const P3_LEFT = 'j';
+export const P3_RIGHT = 'l';
+export const P3_BOOST = 'i';
+export const P4_LEFT = '4';
+export const P4_RIGHT = '6';
+export const P4_BOOST = '8';

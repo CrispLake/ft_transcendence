e3ef0665f6917aea5dab82d58c8fc3e32b8a2d6c
Kinnune <eljas.kinnunen98@gmail.com>
2024-08-22 12:54:27 +0300

    logicFunctions file

--- frontend/app/src/static/js/gonp/logicFunctions.js
+++ frontend/app/src/static/js/gonp/logicFunctions.js
@@ -1,17 +1,22 @@
 
 import * as THREE from 'three';
-import { player1, player2, arena, scene } from './gonp.js'
 import * as G from './globals.js';
+import { game } from './gonp.js'
+
+// let player1 = game.player1;
+// let player2 = game.player2;
+// let scene = game.scene;
+// let arena = game.arena;
 
 export function setPushersColliding(colliding)
 {
-	for (let i = 0; i < player1.pushers.length; i++) {
-		player1.pushers[i].colliding = colliding;
-		// player1.pushers[i].collisionNumber = -1;
+	for (let i = 0; i < game.player1.pushers.length; i++) {
+		game.player1.pushers[i].colliding = colliding;
+		// game.player1.pushers[i].collisionNumber = -1;
 	}
-	for (let i = 0; i < player2.pushers.length; i++) {
-		player2.pushers[i].colliding = colliding;
-		// player2.pushers[i].collisionNumber = -1;
+	for (let i = 0; i < game.player2.pushers.length; i++) {
+		game.player2.pushers[i].colliding = colliding;
+		// game.player2.pushers[i].collisionNumber = -1;
 	}
 }
 
@@ -20,21 +25,21 @@ export function checkPowerupCollision() {
 	let balance = Math.random();
 	let player;
 	let players = [
-		player1,
-		player2
+		game.player1,
+		game.player2
 	];
 	if (balance < 0.5) {
-		players[0] = player2;
-		players[1] = player1;
+		players[0] = game.player2;
+		players[1] = game.player1;
 	}
 	for (let i = 0; i < players.length; i++) {
 		player = players[i];
 		for (let j = 0; j < player.pushers.length; j++) {
-			if (arena.powerup) {
-				if (player.pushers[j].box.intersectsBox(arena.powerup.box)) {
-					scene.remove(arena.powerup.mesh)
-					arena.powerup.remove();
-					arena.powerup = null;
+			if (game.arena.powerup) {
+				if (player.pushers[j].box.intersectsBox(game.arena.powerup.box)) {
+					game.scene.remove(game.arena.powerup.mesh)
+					game.arena.powerup.remove();
+					game.arena.powerup = null;
 					speedup = player.pushers[j].size > G.pusherMaxSize * 0.9;
 					player.pushers[j].upSize(G.pusherMaxSize);
 					if (speedup) {
@@ -49,13 +54,13 @@ export function checkPowerupCollision() {
 function checkPushersCollision() {
 	let collisionNumber = 0;
 
-	for (let i = 0; i < player1.pushers.length; i++) {
-		const pusher1 = player1.pushers[i];
+	for (let i = 0; i < game.player1.pushers.length; i++) {
+		const pusher1 = game.player1.pushers[i];
 		const box1 = pusher1.box;
 		pusher1.collisionNumber = -1;
 
-		for (let j = 0; j < player2.pushers.length; j++) {
-			const pusher2 = player2.pushers[j];
+		for (let j = 0; j < game.player2.pushers.length; j++) {
+			const pusher2 = game.player2.pushers[j];
 			const box2 = pusher2.box;
 			pusher2.collisionNumber = -1;
 
@@ -91,30 +96,30 @@ function checkPushersCollision() {
 // 	let i;
 
 
-// 	for (i = 0; i < player1.pushers.length; i++) {
-// 		collisionNumber = player1.pushers[i].collisionNumber;
+// 	for (i = 0; i < game.player1.pushers.length; i++) {
+// 		collisionNumber = game.player1.pushers[i].collisionNumber;
 // 		if (collisionNumber >= 0) {
 // 			if (velocity[collisionNumber] === undefined) {
 // 				velocity[collisionNumber] = 0;
 // 			}
-// 			velocity[collisionNumber] -= player1.pushers[i].speed;
+// 			velocity[collisionNumber] -= game.player1.pushers[i].speed;
 // 			if (mass[collisionNumber] === undefined) {
 // 				mass[collisionNumber] = 0;
 // 			}
-// 			mass[collisionNumber] -= player1.pushers[i].size;
+// 			mass[collisionNumber] -= game.player1.pushers[i].size;
 // 		}
 // 	}
-// 	for (i = 0; i < player2.pushers.length; i++) {
-// 		collisionNumber = player2.pushers[i].collisionNumber;
+// 	for (i = 0; i < game.player2.pushers.length; i++) {
+// 		collisionNumber = game.player2.pushers[i].collisionNumber;
 // 		if (collisionNumber >= 0) {
 // 			if (velocity[collisionNumber] === undefined) {
 // 				velocity[collisionNumber] = 0;
 // 			}
-// 			velocity[collisionNumber] += player2.pushers[i].speed;
+// 			velocity[collisionNumber] += game.player2.pushers[i].speed;
 // 			if (mass[collisionNumber] === undefined) {
 // 				mass[collisionNumber] = 0;
 // 			}
-// 			mass[collisionNumber] += player2.pushers[i].size;
+// 			mass[collisionNumber] += game.player2.pushers[i].size;
 // 		}
 // 	}
 // 	for (i = 0; i < mass.length; i++) {
@@ -123,14 +128,14 @@ function checkPushersCollision() {
 // 	for (i = 0; i < velocity.length; i++) {
 // 		momentum[i] -= mass[i] * (momentum[i] > 0 ? 1 : -1);
 // 	}
-// 	for (i = 0; i < player1.pushers.length; i++) {
-// 		if (player1.pushers[i].collisionNumber >= 0) {
-// 			player1.pushers[i].moveX(momentum[player1.pushers[i].collisionNumber]);
+// 	for (i = 0; i < game.player1.pushers.length; i++) {
+// 		if (game.player1.pushers[i].collisionNumber >= 0) {
+// 			game.player1.pushers[i].moveX(momentum[game.player1.pushers[i].collisionNumber]);
 // 		}
 // 	}
-// 	for (i = 0; i < player2.pushers.length; i++) {
-// 		if (player2.pushers[i].collisionNumber >= 0) {
-// 			player2.pushers[i].moveX(momentum[player2.pushers[i].collisionNumber]);
+// 	for (i = 0; i < game.player2.pushers.length; i++) {
+// 		if (game.player2.pushers[i].collisionNumber >= 0) {
+// 			game.player2.pushers[i].moveX(momentum[game.player2.pushers[i].collisionNumber]);
 // 		}
 // 	}
 // 	// console.log(velocity, mass, momentum);
@@ -147,59 +152,59 @@ export function pushersLogic() {
 export function movePushers() {
 	let pusher;
 
-	// console.log(player1.pushers)
-	for (let i = 0; i < player1.pushers.length; i++) {
-		pusher = player1.pushers[i];
-		player1.movePusher(pusher);
+	// console.log(game.player1.pushers)
+	for (let i = 0; i < game.player1.pushers.length; i++) {
+		pusher = game.player1.pushers[i];
+		game.player1.movePusher(pusher);
 		if (pusher.furtestX > G.laneEnd) {
-			arena.lanes[pusher.lane].player1scored(pusher.size - (G.pusherMinSize / 2));
-			player1.removePusher(pusher);
+			game.arena.lanes[pusher.lane].player1scored(pusher.size - (G.pusherMinSize / 2));
+			game.player1.removePusher(pusher);
 		}
-		else if (pusher.furtestX < arena.getOpposingSectionPositionByPusher(pusher)) {
-			arena.lanes[pusher.lane].player1scored(G.passiveScore);
+		else if (pusher.furtestX < game.arena.getOpposingSectionPositionByPusher(pusher)) {
+			game.arena.lanes[pusher.lane].player1scored(G.passiveScore);
 			pusher.downSize(G.passiveScore);
 		}
 	}
-	for (let i = 0; i < player2.pushers.length; i++) {
-		pusher = player2.pushers[i];
-		player2.movePusher(pusher)
+	for (let i = 0; i < game.player2.pushers.length; i++) {
+		pusher = game.player2.pushers[i];
+		game.player2.movePusher(pusher)
 		if (pusher.furtestX < -G.laneEnd) {
-			arena.lanes[pusher.lane].player2scored(pusher.size - (G.pusherMinSize / 2));
-			player2.removePusher(pusher);
+			game.arena.lanes[pusher.lane].player2scored(pusher.size - (G.pusherMinSize / 2));
+			game.player2.removePusher(pusher);
 		}
 		// else if (pusher.furtestX > G.laneEnd ) {
-		// 	arena.lanes[pusher.lane].player2scored(pusher.size - (G.pusherMinSize / 2));
-		// 	player2.removePusher(pusher);
+		// 	game.arena.lanes[pusher.lane].player2scored(pusher.size - (G.pusherMinSize / 2));
+		// 	game.player2.removePusher(pusher);
 		// }
-		else if (pusher.furtestX > arena.getOpposingSectionPositionByPusher(pusher)) {
-			arena.lanes[pusher.lane].player2scored(G.passiveScore);
+		else if (pusher.furtestX > game.arena.getOpposingSectionPositionByPusher(pusher)) {
+			game.arena.lanes[pusher.lane].player2scored(G.passiveScore);
 			pusher.downSize(G.passiveScore);
 		}
 	}
 }
 
 export function playersLogic() {
-	player1.logicLoop();
-	player2.logicLoop();
+	game.player1.logicLoop();
+	game.player2.logicLoop();
 }
 
 export function updatePlayerPosition() {
-	player1.move();
-	player2.move();
+	game.player1.move();
+	game.player2.move();
 
 }
 
 export function updateBoost() {
-	if (player1.boostPressed) {
-		player1.increaseBoost();
+	if (game.player1.boostPressed) {
+		game.player1.increaseBoost();
 	}
 	else {
-		player1.resetBoost();
+		game.player1.resetBoost();
 	}
-	if (player2.boostPressed) {
-		player2.increaseBoost();
+	if (game.player2.boostPressed) {
+		game.player2.increaseBoost();
 	}
 	else {
-		player2.resetBoost();
+		game.player2.resetBoost();
 	}
 }

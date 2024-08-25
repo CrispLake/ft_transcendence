8d0c63b84e6a7a6ffd6d5ac00102ab54fcdedf81
CrispLake <eemilmajuri@gmail.com>
2024-08-25 14:17:49 +0300

    Fixed cert problem

--- transcendence_src/start.sh
+++ transcendence_src/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python ./manage.py migrate
-gunicorn --certfile=certs/django.crt --keyfile=certs/django.key --bind 0.0.0.0:8000 transcendence.wsgi:application
+gunicorn --certfile=certs/selfsigned.crt --keyfile=certs/selfsigned.key --bind 0.0.0.0:8000 transcendence.wsgi:application

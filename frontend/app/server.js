f0b6e272053b3545e3e3d0c5829ada5ddf290be3
CrispLake <eemilmajuri@gmail.com>
2024-08-25 14:21:23 +0300

    change

--- frontend/app/server.js
+++ frontend/app/server.js
@@ -6,12 +6,11 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 08:03:51 by jmykkane          #+#    #+#             */
-/*   Updated: 2024/08/25 12:25:33 by emajuri          ###   ########.fr       */
+/*   Updated: 2024/08/25 14:22:05 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 var fs = require('fs');
-// var http = require('http');
 var https = require('https');
 const path = require('path');
 var privateKey  = fs.readFileSync('app/certs/selfsigned.key', 'utf8');
@@ -27,13 +26,6 @@ app.get('/*', (request, response) => {
   response.sendFile(path.resolve(__dirname, 'src', 'index.html'));
 });
 
-// var httpApp = express();
-// httpApp.use((req, res, next) => {
-//   res.redirect('https://' + req.headers.host + req.url);
-// });
-
-// var httpServer = http.createServer(httpApp);
 var httpsServer = https.createServer(credentials, app);
 
-// httpServer.listen(8080);
 httpsServer.listen(3000);
